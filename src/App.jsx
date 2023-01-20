9face63bc4001db5225c74a99c63e885c312af8c
Juan Macías <info@juanmacias.es>
2023-01-20 01:47:44 +0100

    config re style index using Chakra components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import logo from './assets/react.svg';
+import { Box, Flex, Heading, Img } from '@chakra-ui/react';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () =>  {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </div>
+    <Box h="full" bgGradient={{ sm: 'linear(to-r, blue.600, purple.600)' }}>
+      <Flex h="full" flexDir="column" justify="center" align="center" textAlign="center" gap={10}>
+        <Img w={36} src={logo} color="primary.1000" />
+        <Heading as="h1">React Awesome</Heading>
+      </Flex>
+    </Box>
   );
 }
 

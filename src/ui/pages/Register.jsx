2f987760fdb620af3ce88261ce06613ed510672a
Juan Macías <info@juanmacias.es>
2023-01-20 03:14:33 +0100

    config change register

--- src/ui/pages/Register.jsx
+++ src/ui/pages/Register.jsx
@@ -12,73 +12,88 @@ import {
   Text,
   useBreakpointValue,
 } from '@chakra-ui/react';
-// import { useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 
 const Register = () => {
-//   const { register, handleSubmit } = useForm();
+  const { register, handleSubmit } = useForm();
+
+  const submit = result => {
+    console.log('result', result);
+  };
 
   return (
     <Container
       maxW="md"
       py={{
-        base: '12',
-        md: '24',
+        base: '6',
+        md: '8',
       }}
     >
-      <Stack spacing="8">
-        <Stack spacing="6" align="center">
-          <Img src={logo} />
-          <Stack spacing="3" textAlign="center">
-            <Heading
-              color="white"
-              size={useBreakpointValue({
-                base: 'md',
-                md: 'xl',
-              })}
-            >
-              Crea tu cuenta
-            </Heading>
-            <Text color="gray.200">Empieza a hacer realidad tus sueños</Text>
+      <form onSubmit={handleSubmit(submit)}>
+        <Stack spacing="8">
+          <Stack spacing="6" align="center">
+            <Img src={logo} />
+            <Stack spacing="3" textAlign="center">
+              <Heading
+                color="white"
+                size={useBreakpointValue({
+                  base: 'md',
+                  md: 'xl',
+                })}
+              >
+                Crea tu cuenta
+              </Heading>
+              <Text color="gray.200">Empieza a hacer realidad tus sueños</Text>
+            </Stack>
           </Stack>
+          <Stack spacing="6">
+            <Stack spacing="5">
+              <FormControl isRequired>
+                <FormLabel color="white" htmlFor="name">
+                  Name
+                </FormLabel>
+                <Input bg="gray.100" id="name" type="text" {...register('name')} focusBorderColor="primary.300" />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel color="white" htmlFor="email">
+                  Email
+                </FormLabel>
+                <Input bg="gray.100" id="email" type="email" {...register('email')} focusBorderColor="primary.300" />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel color="white" htmlFor="password">
+                  Password
+                </FormLabel>
+                <Input
+                  bg="gray.100"
+                  id="password"
+                  type="password"
+                  {...register('password')}
+                  focusBorderColor="primary.300"
+                />
+                <FormHelperText color="gray.200">
+                  Como mínimo 12 carácteres de longitud, 1 número y 1 símbolo
+                </FormHelperText>
+              </FormControl>
+            </Stack>
+            <Stack spacing="2">
+              <Button type="submit" variant="solid">
+                Crear cuenta
+              </Button>
+            </Stack>
+          </Stack>
+          <HStack justify="center" spacing="2">
+            <Text fontSize="sm" color="white">
+              Ya estás registrado?
+            </Text>
+            <Button as={Link} to="/login" variant="link" size="sm">
+              Log in
+            </Button>
+          </HStack>
         </Stack>
-        <Stack spacing="6">
-          <Stack spacing="5">
-            <FormControl isRequired>
-              <FormLabel color="white" htmlFor="name">
-                Name
-              </FormLabel>
-              <Input bg="gray.100" id="name" type="text" />
-            </FormControl>
-            <FormControl isRequired>
-              <FormLabel color="white" htmlFor="email">
-                Email
-              </FormLabel>
-              <Input bg="gray.100" id="email" type="email" />
-            </FormControl>
-            <FormControl isRequired>
-              <FormLabel color="white" htmlFor="password">
-                Password
-              </FormLabel>
-              <Input bg="gray.100" id="password" type="password" />
-              <FormHelperText color="gray.200">
-                Como mínimo 12 carácteres de longitud, 1 número y 1 símbolo
-              </FormHelperText>
-            </FormControl>
-          </Stack>
-          <Stack spacing="4">
-            <Button variant="solid">Create account</Button>
-          </Stack>
-        </Stack>
-        <HStack justify="center" spacing="1">
-          <Text fontSize="sm" color="white">
-            Already have an account?
-          </Text>
-          <Button variant="link" colorScheme="primary" size="sm">
-            Log in
-          </Button>
-        </HStack>
-      </Stack>
+      </form>
     </Container>
   );
 };

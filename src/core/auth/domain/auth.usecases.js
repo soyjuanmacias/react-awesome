e8aef4e8f71cea96fd4ab56be34fea178b8884a7
Juan Macías <info@juanmacias.es>
2023-01-21 06:33:25 +0100

    feat: now, navigate from register when complete form

--- src/core/auth/domain/auth.usecases.js
+++ src/core/auth/domain/auth.usecases.js
@@ -1,9 +1,14 @@
+import { useNavigate } from 'react-router';
 import * as service from './auth.service';
 
 const authUseCases = () => {
+  const navigate = useNavigate();
   const login = credentials => service.login(credentials);
 
-  const register = user => service.register(user);
+  const register = async user => {
+    await service.register(user);
+    navigate('/');
+  }
 
   const retrieveUser = () => service.retrieveUser();
 

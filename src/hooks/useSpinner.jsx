1957ac3cae0b68029a23bfbe9f66d9b9256f48c1
Juan Macías <info@juanmacias.es>
2023-01-21 06:32:47 +0100

    feat: useSpinner hook created

--- src/hooks/useSpinner.jsx
+++ src/hooks/useSpinner.jsx
@@ -1,5 +1,15 @@
+import { useSelector } from "react-redux";
+
 const useSpinner = () => {
-    
+    const loginSpinner = useSelector(state => state.global.spinners.includes('login'));
+    const registerSpinner = useSelector(state => state.global.spinners.includes('register'));
+    const retrieveUserSpinner = useSelector(state => state.global.spinners.includes('retrieveUser'));
+
+    return {
+        loginSpinner,
+        registerSpinner,
+        retrieveUserSpinner,
+    }
 };
 
 export default useSpinner;

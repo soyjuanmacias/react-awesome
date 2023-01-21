c47aaf001b6ecf4e6baf40473e8162d77c427dd3
Juan Macías <info@juanmacias.es>
2023-01-21 02:35:46 +0100

    feat: create http service for handling requests

---
+++ src/core/global/services/http.service.js
@@ -0,0 +1,62 @@
+import axios from 'axios';
+
+const { MODE } = import.meta.env;
+
+/**
+ * Provider: axios, fetch...
+ * baseUrl: (server route) https://localhost:3000
+ * prefix: if api has prefix (in our case /api/v1/)
+ */
+class HttpFactory {
+  constructor(provider, baseUrl, prefix = '') {
+    this.provider = provider;
+    this.baseUrl = baseUrl;
+    this.url = `${baseUrl}${prefix}`;
+  }
+
+  setConfig() {
+    return {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': this.baseUrl,
+      },
+      withCredentials: true,
+      timeout: 3000,
+    };
+  }
+
+  request(path, options, data) {
+    const config = { url: this.url + path, data, ...this.setConfig(), ...options };
+
+    return this.provider(config)
+      .then(res => res.data)
+      .catch(error => console.log('Error en el axios', error.response.data));
+    // TODO Crear Interceptor de errores siguiendo Interceptor Error;
+  }
+
+  get(path, options) {
+    return this.request(path, { method: 'get', ...options });
+  }
+
+  post(path, data, options) {
+    return this.request(path, data, { method: 'post', ...options });
+  }
+
+  put(path, data, options) {
+    return this.request(path, data, { method: 'put', ...options });
+  }
+
+  delete(path, options) {
+    return this.request(path, { method: 'delete', ...options });
+  }
+}
+
+// TODO create enviroments and get here auto back url
+
+const servers = {
+  dev: 'http://localhost:4000',
+  prod: 'TODOTODOTOD',
+};
+
+export const http = new HttpFactory(axios, servers[MODE], '/api/v1');
